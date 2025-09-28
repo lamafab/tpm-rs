1f130654721ba389e8caf0c99f05751a8f5254d7
Fabio Lama <fabio.lama@pm.me>
2025-09-28 18:57:22 +0000

    wip

--- clientv2/src/tests/mod.rs
+++ clientv2/src/tests/mod.rs
@@ -1,9 +1,12 @@
+use std::fs;
+
 use crate::{
-    algo::{AlgoSha256, AlgoSha256Hasher, AlgoSha256Hmac},
+    algo::{AlgoSha256, AlgoSha256Hmac},
     crypto::session_key,
     session::HmacSession,
     tpm::{run_command_with_handles, FileIoTpm},
 };
+use rsa::{pkcs8::DecodePublicKey, Pkcs1v15Encrypt, RsaPublicKey};
 use tpm2_rs_base::{
     commands::{CreatePrimaryCmd, StartAuthSessionCmd, StartAuthSessionHandles},
     constants::TpmSe,
@@ -37,6 +40,112 @@ fn test_start_auth_create_primary() {
 
     let cmd_handles = StartAuthSessionHandles {
         tpm_key: TpmiDhObject::RHNull,
+        bind: TpmiDhEntity::RHOwner,
+    };
+
+    let mut cmd_session = ();
+
+    // ### Execute `TPM2_StartAuthSession` command!
+    let (resp, session_handle) =
+        run_command_with_handles(&cmd, &cmd_handles, &mut cmd_session, &mut tpm).unwrap();
+
+    // ## Prepare payload for `TPM2_CreatePrimary`
+
+    // Use empty auth, generate random sensitive data.
+    let in_sensitive = Tpm2bSensitiveCreate::from_struct(&TpmsSensitiveCreate {
+        user_auth: Tpm2bAuth::from_bytes(&[]).unwrap(),
+        data: Tpm2bSensitiveData::from_bytes(&rand::random::<[u8; 32]>()).unwrap(),
+    })
+    .unwrap();
+
+    let object_attributes = TpmaObject::FIXED_TPM
+        | TpmaObject::FIXED_PARENT
+        | TpmaObject::USER_WITH_AUTH
+        | TpmaObject::SIGN_ENCRYPT;
+
+    // Use empty auth policy.
+    let auth_policy = Tpm2bDigest::from_bytes(&[]).unwrap();
+
+    let parms_and_id = PublicParmsAndId::KeyedHash(
+        TpmsKeyedHashParms {
+            scheme: TpmtKeyedHashScheme::Hmac(TpmsSchemeHash {
+                hash_alg: TpmiAlgHash::SHA256,
+            }),
+        },
+        Tpm2bDigest::from_bytes(&[]).unwrap(),
+    );
+
+    let in_public = Tpm2bPublic::from_struct(&TpmtPublic {
+        name_alg: TpmiAlgHash::SHA256,
+        object_attributes,
+        auth_policy,
+        parms_and_id,
+    })
+    .unwrap();
+
+    // Use empty outside info, use empty creation PCR.
+    let outside_info = Tpm2bData::from_bytes(&[]).unwrap();
+    let creation_pcr = TpmlPcrSelection::new(&[]).unwrap();
+
+    let cmd = CreatePrimaryCmd {
+        in_sensitive,
+        in_public,
+        outside_info,
+        creation_pcr,
+    };
+
+    let session_key =
+        session_key::<AlgoSha256Hmac>(&[], &[], &resp.nonce_tpm, &nonce_caller).unwrap();
+
+    let mut session = HmacSession::<AlgoSha256>::new(
+        auth_val,
+        session_handle,
+        TpmaSession::CONTINUE_SESSION,
+        Some(session_key),
+        resp.nonce_tpm,
+    );
+
+    // ### Execute `TPM2_CreatePrimary` command!
+    let cmd_handle = TpmiRhHierarchy::TpmRhOwner;
+    let (resp, handle) =
+        run_command_with_handles(&cmd, &cmd_handle, &mut session, &mut tpm).unwrap();
+
+    let (resp, handle) =
+        run_command_with_handles(&cmd, &cmd_handle, &mut session, &mut tpm).unwrap();
+}
+
+#[test]
+fn test_start_auth_create_primary_with_rsa_encryption() {
+    // Use the specified TPM, with and empty auth key!
+    let mut tpm = FileIoTpm::new("/dev/tpmrm0").unwrap();
+    let auth_val = vec![];
+
+    // Setup the TPM's RSA public key.
+    let rsa_pem = fs::read_to_string("../ek_public.pem").unwrap();
+    let rsa = RsaPublicKey::from_public_key_pem(&rsa_pem).unwrap();
+
+    // ## Prepare payload for `TPM2_StartAuthSession`
+
+    // Generate random nonce, use empty salt.
+    let nonce_caller = Tpm2bNonce::from_bytes(&rand::random::<[u8; 32]>()).unwrap();
+
+    let mut rng = rsa::rand_core::OsRng;
+    let salt = rand::random::<[u8; 20]>();
+    let oaep = rsa::Oaep::new::<sha2::Sha256>();
+    let encrypted_salt = rsa.encrypt(&mut rng, oaep, &salt).unwrap();
+
+    let encrypted_salt = Tpm2bEncryptedSecret::from_bytes(&encrypted_salt).unwrap();
+
+    let cmd = StartAuthSessionCmd {
+        nonce_caller,
+        encrypted_salt,
+        session_type: TpmSe::HMAC,
+        symmetric: tpm2_rs_base::TpmtSymDefObject::Null(TpmsEmpty, TpmsEmpty),
+        auth_hash: TpmiAlgHash::SHA256,
+    };
+
+    let cmd_handles = StartAuthSessionHandles {
+        tpm_key: TpmiDhObject::RHOwner,
         bind: TpmiDhEntity::RHOwner,
     };
 
